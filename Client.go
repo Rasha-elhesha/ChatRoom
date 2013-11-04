efb73e0566b078fba58dff34742b5c7e931446dc
Rasha-elhesha <rasha_assad89@yahoo.com>
2013-11-04 03:05:55 -0500

    Update Client.go
    More interaction with the server

--- Client.go
+++ Client.go
@@ -2,18 +2,31 @@ package main
 
 import (
     "net"
-	"time"
+	"fmt"
+	"bufio"
+	"os"
 )
 
 func main() {
-    c,err := net.Dial("tcp", "localhost:8080")
+    connection,err := net.Dial("tcp", "localhost:8080")
     if err != nil {
         panic("Error")
     }
-    defer c.Close()
-    _,err := c.Write([]byte("hi, I am the client"))
-    if err != nil {
-        println("Error write")
-    }
-    time.Sleep(10)
+    defer connection.Close()
+	//send the user name
+	fmt.Printf("Welcome to chat rooms. Please enter your name.\n")
+	inputReader := bufio.NewReader(os.Stdin)
+	name, _ , _:= inputReader.ReadLine()
+	userName := string(name)
+    connection.Write([]byte(userName))
+	
+	
+	//asks the user if want to join a session or want to new  one
+	fmt.Printf("What would you like to do?\n1.Start a new session.\n2.Join an existing one.\n")
+	option, _ := inputReader.ReadString('\n')
+    connection.Write([]byte(option))
+	
+    //run the reader to read any messages received from the server
+	
+	//run the writer to read messages from console and send them to the server
 }
