4c428b2bc87483186dd1a421a810e941e04f4548
Rasha-elhesha <rasha_assad89@yahoo.com>
2013-11-05 10:19:47 -0500

    Update Client.go
    Added functionality to support to read messages from  server and write messages to the server

--- Client.go
+++ Client.go
@@ -7,6 +7,8 @@ import (
 	"os"
 )
 
+
+
 func main() {
     connection,err := net.Dial("tcp", "localhost:8080")
     if err != nil {
@@ -19,14 +21,37 @@ func main() {
 	name, _ , _:= inputReader.ReadLine()
 	userName := string(name)
     connection.Write([]byte(userName))
-	
-	
+		
 	//asks the user if want to join a session or want to new  one
 	fmt.Printf("What would you like to do?\n1.Start a new session.\n2.Join an existing one.\n")
 	option, _ := inputReader.ReadString('\n')
     connection.Write([]byte(option))
 	
-    //run the reader to read any messages received from the server
-	
 	//run the writer to read messages from console and send them to the server
+	go messageWriter(connection)
+	
+    //run the reader to read any messages received from the server
+	messageReader(connection)
+	
+}
+
+func messageReader(client net.Conn) {
+	inputReader := bufio.NewReader(os.Stdin)
+	for {
+		message, _ := inputReader.ReadString('\n')
+		client.Write([]byte(message))
+	}
+}
+
+func messageWriter(client net.Conn) {
+	reader := bufio.NewReader(client)
+	for {
+		message, err := reader.ReadBytes('\n')
+        if err != nil { // EOF, or worse
+		fmt.Printf("Error read from server")
+            break
+        }
+		//write the received message
+		fmt.Printf(string(message))
+	}
 }
