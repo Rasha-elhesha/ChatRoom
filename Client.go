e992f53c44e7d628e187cb5ef686de94a09737ac
Rasha-elhesha <rasha_assad89@yahoo.com>
2013-11-07 03:14:12 -0500

    Update Client.go
    Added some fault tolerance features

--- Client.go
+++ Client.go
@@ -11,16 +11,14 @@ import (
 
 func main() {
     connection,err := net.Dial("tcp", "localhost:8080")
+	//Close connection and clean up when the client finish chat
     if err != nil {
-        panic("Error")
+        fmt.Println("Can not connect to the server")
+		return
     }
-    defer connection.Close()
-	//send the user name
-	fmt.Printf("Welcome to chat rooms. Please enter your name.\n")
-	inputReader := bufio.NewReader(os.Stdin)
-	name, _ , _:= inputReader.ReadLine()
-	userName := string(name)
-    connection.Write([]byte(userName))
+	defer cleanUp(connection)
+	//send the user name to the server
+	sendUserName(connection)
 	
 	fmt.Printf("*****************Go Chat*****************\n")
 	//run the writer to read messages from console and send them to the server
@@ -31,20 +29,61 @@ func main() {
 	
 }
 
+/*
+ * This function is called when error occurred during runtime
+ * The function closes the connection made with the server
+*/
+func cleanUp(clientConnection net.Conn) {
+	clientConnection.Close()
+	os.Exit(0)
+}
+
+/*
+ * This function is called by the main after the connection made to the server
+ * The function request the user to enter his name and sends this name to the server
+*/
+func sendUserName(client net.Conn) {
+	fmt.Printf("Welcome to chat rooms. Please enter your name.\n")
+	inputReader := bufio.NewReader(os.Stdin)
+	name, _ , error:= inputReader.ReadLine()
+	if error != nil {
+        fmt.Println("Can not read user name")
+		cleanUp(client)
+		return
+    }
+	userName := string(name)
+    client.Write([]byte(userName))
+}
+
+/*
+ * This function is called by the main as part of chatting functionality
+ * The function waits for user input message  and sends this message to the server to broadcast it
+*/
 func messageReader(client net.Conn) {
 	inputReader := bufio.NewReader(os.Stdin)
 	for {
-		message, _ := inputReader.ReadString('\n')
+		message, error := inputReader.ReadString('\n')
+		if error != nil {
+			fmt.Println("Can not read user message")
+			cleanUp(client)
+			break
+		}
+		//Send the user message to the server
 		client.Write([]byte(message))
 	}
 }
 
+/*
+ * This function is called by the main as part of chatting functionality
+ * The function waits the server to send a broadcasted message and print this message in the terminal
+*/
 func messageWriter(client net.Conn) {
 	reader := bufio.NewReader(client)
 	for {
 		message, err := reader.ReadBytes('\n')
-        if err != nil { // EOF, or worse
-		fmt.Printf("Error read from server")
+        if err != nil {
+			fmt.Println("Error read from server")
+			cleanUp(client)
             break
         }
 		//write the received message
